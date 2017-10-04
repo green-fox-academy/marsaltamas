6f3a8662c596d794f47014b19f5e935a42a25126
Tamas Marsal <tamas.marsal@gmail.com>
2017-10-04 15:37:04 +0200

    Finished task 18.

---
+++ week-01/day-3/c-workshop/18.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int main() {
+	uint16_t number = 260;
+	uint8_t eigth_bit_max = 0b11111111;
+
+	printf("eight_bit_max is %d\n", eigth_bit_max);
+
+	if (number > eigth_bit_max)
+        printf("longer than 8bits");
+    else
+        printf("ok");
+
+	// if number can't be represented with 8bits
+	// print 'longer than 8bits' otherwise print 'ok'
+	return 0;
+}
