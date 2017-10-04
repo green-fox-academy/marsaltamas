cd5878e1cf6658ab2a7da4512164163893dcf066
Tamas Marsal <tamas.marsal@gmail.com>
2017-10-04 13:31:25 +0200

    Finished task 10

---
+++ week-01/day-3/c-workshop/10.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	double j1 = 10;
+	double j2 = 3;
+	// tell if j1 is higher than j2 squared and smaller than j2 cubed
+
+	printf("j1:%.2f\nj2:%.2f\nj2squared:%.2f\nj2cubed:%.2f\n", j1, j2, pow(j2, 2), j2*j2*j2);
+
+	if ((j1 > pow(j2, 2)) && j1 < pow(j2, 3)){
+
+        printf("j1 is higher than j2 squared and smaller than j2 cubed.");
+
+     }
+
+	return 0;
+}
