9cf558da1d84ec0c1eb9566a915b5b680514c5f5
Tamas Marsal <tamas.marsal@gmail.com>
2017-10-04 15:30:37 +0200

    Styled task 9 and finished task 17.

--- week-01/day-3/c-workshop/09.c
+++ week-01/day-3/c-workshop/09.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 
 int main() {
-	int i = 53625;;
+	int i = 53625;
 	// tell if it has 11 as a divisor
 
 	printf("i= %d / 11 = %d\n", i, i/11);
 	printf("i= %d %c 11 = %d\n", i, 37, i%11);
 
-	if((i%11)==0){
+	if(!(i%11))
         printf("%d has 11 as a divisor", i);
-	}
-	else{
+	else
         printf("%d does not have 11 as a divisor", i);
-	}
+
 
 
 	return 0;
---
+++ week-01/day-3/c-workshop/17.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+	float w = 24;
+	int out = 0;
+
+	// if w is even increment out by one
+
+	if ( ! ((int) w % 2))
+        out += 1;
+
+
+
+        printf("%d", out);
+
+
+
+
+	return 0;
+}
