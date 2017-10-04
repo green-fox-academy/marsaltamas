7b9a5f4b68de21da808030cfe72b7ef782c1afa0
Tamas Marsal <tamas.marsal@gmail.com>
2017-10-04 13:57:45 +0200

    modified task 9 printf and finished task 12

--- week-01/day-3/c-workshop/09.c
+++ week-01/day-3/c-workshop/09.c
@@ -5,7 +5,7 @@ int main() {
 	// tell if it has 11 as a divisor
 
 	printf("i= %d / 11 = %d\n", i, i/11);
-	printf("i= %d mod 11 = %d\n", i, i%11);
+	printf("i= %d %c 11 = %d\n", i, 37, i%11);
 
 	if((i%11)==0){
         printf("%d has 11 as a divisor", i);
---
+++ week-01/day-3/c-workshop/12.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int main() {
+    uint8_t a = 60;
+    uint8_t b = 0b00111100;
+    uint8_t c = 0x3C;
+    uint8_t d = 0x3c;
+
+    printf("------------------\n", d);
+    printf("%d\n", a);
+    printf("%d\n", b);
+    printf("%d\n", c);
+    printf("%d\n", d);
+    printf("------------------\n", d);
+
+    // Try with %x
+
+
+
+    // Multiply a by 2
+    a = a * 2;
+
+    // Shift left b by 1
+    b = b << 1;
+
+    // Check their values
+    printf("%d\n", a);
+    printf("%d\n", b);
+    printf("%8x\n", c);
+    printf("%08x\n", d);
+    printf("------------------\n", d);
+
+    return 0;
+}
