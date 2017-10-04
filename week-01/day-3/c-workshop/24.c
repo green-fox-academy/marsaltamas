4686e005e6e5d7c4239aa7bbe403c73e47ff5d28
Tamas Marsal <tamas.marsal@gmail.com>
2017-10-04 16:19:48 +0200

    Finished task 24.

---
+++ week-01/day-3/c-workshop/24.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+Create a program which converts days to years, months, weeks and days
+to keep it simple we say every year is 365 days long, every month is 30 days
+*/
+
+int main()
+{
+    int days = 397;
+
+    printf("%d days is %d years, %d months and %d days in total, if all months are 30 days long, and a year is 365 days.", days, days/365, days%365/30, days%365%30);
+
+    return 0;
+}
