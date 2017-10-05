6f2fcfe9285584524e6fc7d890ee338c52ee3faf
Tamas Marsal <tamas.marsal@gmail.com>
2017-10-05 11:26:11 +0200

    Finished task 7.

---
+++ week-01/day-4/c-workshop-2/07.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+create a simple program which checks if the num is odd or even
+*/
+
+int main()
+{
+    int num;
+    num = -2;
+
+    int result = num % 2;
+
+    if (num == 0)
+    {
+        printf("num = %d.", num);
+    }
+    else
+    {
+    switch (result){
+    case 0 :
+        printf("num = %d is even.", num);
+        break;
+    case 1 :
+        printf("num = %d is odd.", num);
+        break;
+    default :
+        printf("def");
+    }
+    }
+
+    return 0;
+}
