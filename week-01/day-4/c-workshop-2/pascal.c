e6839d600c0f333acb038aa71ed4bd539950a859
Tamas Marsal <tamas.marsal@gmail.com>
2017-10-05 18:02:36 +0200

    Made pascal place spaces correctly.

--- week-01/day-4/c-workshop-2/pascal.c
+++ week-01/day-4/c-workshop-2/pascal.c
@@ -17,29 +17,33 @@ create a program which draws a diamond, like this:
 
     int i = 1;
     for (i; i<5; i++){
-        int j = 1;
-        for (j; j<8; j++){
+
+        for (int j = 1; j<4+i; j++){
 
                 if((j<8-(7/2+i)) || (j>((7/2+i))))
-                         printf("%c", '-');
+                         printf("%c", ' ');
                 else
                         printf("%c", '*');
+
+                printf("%c", ' ');
+
 
         }
         printf("\n");
 
     }
 
-
     int x = 4;
     for (x; x>0; x--){
-            int j = 7;
-            for (j; j>0; j--){
+
+            for (int j = 2 + i; j>0; j--){
 
                     if((j<8-(7/2+x)) || (j>((7/2+x))))
-                             printf("%c", '-');
+                             printf("%c", ' ');
                     else
                             printf("%c", '*');
+
+                    printf("%c", ' ');
 
             }
             printf("\n");
