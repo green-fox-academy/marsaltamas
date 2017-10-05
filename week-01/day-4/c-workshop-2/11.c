4e5859a76ef3553471ecfadef991f8b540bb415e
Tamas Marsal <tamas.marsal@gmail.com>
2017-10-05 13:02:01 +0200

    Finished task 11.

---
+++ week-01/day-4/c-workshop-2/11.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+/*
+Write a program that prints the numbers from 1 to 100.
+But for multiples of three print "Fizz" instead of the number
+and for the multiples of five print "Buzz".
+For numbers which are multiples of both three and five print "FizzBuzz".
+But this time, use switch
+*/
+
+int main() {
+
+    int a = 1;
+    int result;
+
+	for (int i = 1; i <= 100; i++){
+         printf("for loop FizzBuzz  ");
+
+        if ( i%3 == 0 && i%5 == 0 )
+            result = 1;
+        else if ( i%3 == 0 )
+           result = 2;
+        else if ( i%5 == 0 )
+            result = 3;
+        else
+            result = 4;
+
+
+        switch (result){
+    case 1 :
+        printf("FizzBuzz\n");
+        break;
+    case 2 :
+        printf("Fizz\n");
+        break;
+    case 3 :
+        printf("Buzz\n");
+        break;
+    default :
+        printf("i = %d\n", i);
+
+
+        }
+
+    }
+
+
+
+    return 0;
+}
