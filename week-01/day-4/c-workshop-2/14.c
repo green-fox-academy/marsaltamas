7143532bef8a9baaeec3a67d591c847755e72144
Tamas Marsal <tamas.marsal@gmail.com>
2017-10-05 13:43:46 +0200

    Finished task 14.

---
+++ week-01/day-4/c-workshop-2/14.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/*
+modify the previous program to print the table reverse order
+for example:
+Fahrenheit       Celsius
+300               148.9
+280               137.8
+260               126.7
+240               115.6
+...
+print out at least 10 lines
+*/
+
+int main() {
+
+
+    float fahrenheit;
+    float celsius;
+
+
+   for (int i = 110; i > 0; i-=10){
+
+    fahrenheit = i;
+    celsius = ((fahrenheit - 32)/1.8);
+
+    printf("Fahrenheit = %.2f\tCelsius = %.2f\n", fahrenheit, celsius);
+
+   }
+
+
+	return 0;
+}
