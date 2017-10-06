116cb1947b27c8749f3176913fa8293209b9c447
Tamas Marsal <tamas.marsal@gmail.com>
2017-10-06 12:00:34 +0200

    Styled, polished, and reformatted armstrong_number.c to work without using pow function.

--- week-01/day-5/armstrong_numbers.c
+++ week-01/day-5/armstrong_numbers.c
@@ -3,60 +3,66 @@
 
 int main(){
 
+    // prompting for user input
 
-int test_number = 10;
-int digits = 0;
-int number_to_count_digits = test_number;
-int check_number = test_number;
-double result = 0;
+    int number_to_test = 0;
 
-// counting digits using modulo
+    printf("Enter a number to test, than press enter to check: ");
+    scanf("%d", &number_to_test);
 
+    int digits = 0;
 
+    // temp holder used to test to number of digits
+    int digit_test_num = number_to_test;
+    // saving input value to compare with program result at the end
+    int check_number = number_to_test;
+    // this will store the result of the calculations
+    int result = 0;
+    // used to calculate and hold the sub result of each digit after pow-ing it
+    int sub_result = 1;
 
-      while( number_to_count_digits > 0 ) {
+    // counting digits using modulo
 
-    number_to_count_digits = (number_to_count_digits - (number_to_count_digits % 10))/10;
-    digits++;
+    while (digit_test_num > 0) {
+
+        digit_test_num = (digit_test_num - (digit_test_num % 10)) / 10;
+        digits++;
    }
 
-   for (int i = 0; i <= digits; i++){
 
-    int to_pow = test_number % 10;
-    double double_to_pow = (double) to_pow;
-    double digits_in_double = (double) digits;
+    for (int i = 1; i <= digits; i++){
 
+       int int_to_pow = number_to_test % 10;
 
-    result =  result + pow ( double_to_pow, digits_in_double);
-    test_number = (test_number - test_number % 10)/10;
+       // setting number to pow
 
+        sub_result = int_to_pow;
+
+        // calculating pow of given digit
+
+        for (int i = 1; i < digits; i++) {
+
+            sub_result = sub_result * int_to_pow;
+        }
+
+        result = result + sub_result;
+
+        //    pow function was used originally to test the number
+        //    double double_to_pow = (double) int_to_pow;
+        //    double digits_in_double = (double) digits;
+        //    result =  result + pow ( double_to_pow, digits_in_double);
+
+        //    updating the test number to know
+
+        number_to_test = (number_to_test - number_to_test % 10) / 10;
 
    }
 
-   if (result == check_number)
-            printf( "It is an Armstrong number.");
+    if (result == check_number)
+        printf("It is an Armstrong number.");
     else
-            printf( "It is not an Armstrong number.");
+        printf("It is not an Armstrong number.");
 
-
-printf( "\nnumber of digits %d\n", digits);
-
-printf( "%d\n", test_number);
-
-
-test_number = (test_number - test_number % 10)/10;
-
-printf( "%d\n", test_number);
-
-
-test_number = (test_number - test_number % 10)/10;
-
-printf( "%d\n", test_number);
-
-
-test_number = (test_number - test_number % 10)/10;
-
-printf( "%d\n", test_number);
 
 return 0;
 
