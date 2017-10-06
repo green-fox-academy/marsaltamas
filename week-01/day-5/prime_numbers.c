ee26972fd0c5b4c78430d296ed4aa9377e0ae9b8
Tamas Marsal <tamas.marsal@gmail.com>
2017-10-06 12:26:00 +0200

    prime_number.c styled up and added restrictions for range to improved reliability.

--- week-01/day-5/prime_numbers.c
+++ week-01/day-5/prime_numbers.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 
 /*
@@ -13,42 +12,43 @@ output :
 
 int main()
 {
-    int from = 2;
-    int to = 100;
+    int from = 11;
+    int to = 34;
     int prime_counter = 0;
+    // this checks if the given number is prime
     int prime = 0;
 
-    if (from == 1)
+    // conditions for ranges and number of 2
+    if (from == 1 && to > 1) {
+
         printf("2 is a prime number.\n");
+        prime_counter++;
+    }
 
     for (int number = from + 1; number < to ; number++){
 
-        if(prime){
+        //  if the previous number was prime (prime = 1), counter increased, message printed, and prime set back to null
+        if(prime) {
         prime_counter++;
         printf("%d is a prime number.\n", number-1);
         prime = 0;
-}
-
+        }
 
         for (int i = 2; i < number; i++){
 
 
+            // if a divisor is found for number i, prime is set to null and loop is quited,
+            if(number % i == 0 ) {
+                    prime = 0;
+                    break;
 
-        if(number % i == 0 ) {
-            prime = 0;
-            break;
-          //  break;
+            }
 
-        }
+        // if divisor not found, prime is set to 1 to increment prime_counter at the beginning of the loop
         prime = 1;
-
+        }
     }
 
-    }
-
-    if (from == 1)
-   printf("Number of primes %d.", prime_counter+1);
-    else
          printf("Number of primes %d.", prime_counter);
 
    return 0;
