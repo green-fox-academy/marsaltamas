243f16b9830504d6eb93181373002f6acbb6605d
Tamas Marsal <tamas.marsal@gmail.com>
2017-10-09 20:52:21 +0200

    Created another version for Variables (Variables2) with two .c files.

---
+++ week-02/day-1/Variables2/func_def.c
@@ -0,0 +1,38 @@
+#include "variables.h"
+
+void get_input()
+{
+    printf("Please enter an integer: ");
+    scanf("%d", &input);
+    printf("You entered: %d.\n", input);
+}
+
+int add_10 (int num1){
+
+    op_done++;
+    return num1 + 10;
+}
+
+int add_15 (int num1)
+{
+    op_done++;
+    return num1 + 15;
+}
+
+int add_22 (int num1)
+{
+    op_done++;
+    return num1 + 22;
+}
+
+int sub_22 (int num1)
+{
+    op_done++;
+    return num1 - 22;
+}
+
+int sub_34 (int num1)
+{
+    op_done++;
+    return num1 - 22;
+}
---
+++ week-02/day-1/Variables2/variables.h
@@ -0,0 +1,16 @@
+#ifndef VARIABLES_HEADER
+#define VARIABLES_HEADER
+
+#include <stdio.h>
+
+int input;
+int op_done;
+void get_input ();
+int add_10 (int num1);
+int add_15(int num1);
+int add_22(int num1);
+int sub_22 (int num1);
+int sub_34 (int num1);
+
+#endif // VARIABLES_HEADER
+
