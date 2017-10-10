60af1559c40a04198b4d1998eb386ffdff6a3a85
Tamas Marsal <tamas.marsal@gmail.com>
2017-10-10 16:21:06 +0200

    first working version of Advanced_4.

--- week-02/day-2/Advanced_4/main.c
+++ week-02/day-2/Advanced_4/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <limits.h>
 
+void array_reverser (int arr[], int size);
+void array_printer (int arr[], int size);
+
 int main()
 {
     //write a program which loads up fully a 50-sized integer array starting from 200 in descending order
@@ -14,7 +17,6 @@ int main()
 
         if (!(i % 85)) {
             my_array[count] = i;
-            printf("#%d: Common divisor of 5 and 17 found at %d.\n", count, i);
             count++;
         }
 
@@ -23,12 +25,36 @@ int main()
         }
     }
 
-    for (int i = 0; i < sizeof(my_array) / sizeof(my_array[0]); i++) {
-
-        printf("Position #%d in my_array is %d\n", i, my_array[i]);
-    }
-
-
+    array_printer(my_array, sizeof(my_array) / sizeof(my_array[1]));
+    array_reverser(my_array, sizeof(my_array) / sizeof(my_array[1]));
+    array_printer(my_array, sizeof(my_array) / sizeof(my_array[1]));
 
     return 0;
+}
+
+void array_reverser (int arr[], int size)
+{
+    int temp = 0;
+    for (int i = 0; i < size/2; i++) {
+
+        temp = arr[i];
+        arr[i] = arr[size - 1 - i];
+        arr[size - 1 - i] = temp;
+    }
+
+    printf("\nI have reversed the array\n");
+}
+
+void array_printer (int arr[], int size)
+{
+
+    printf("\n\n------------------\n");
+    printf("The array printed:\n\n");
+    for (int i = 0; i < size; i++) {
+
+    printf("Position #%d in my_array is %d\n", i, arr[i]);
+    }
+    printf("\nEnd of your array\n");
+    printf("------------------\n");
+
 }
