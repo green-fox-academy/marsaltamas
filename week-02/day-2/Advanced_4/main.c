5bd38959ff8a4a939c107e30efadb9bfd6a4305e
Tamas Marsal <tamas.marsal@gmail.com>
2017-10-10 15:36:43 +0200

    set up initial array with serched numbers.

--- week-02/day-2/Advanced_4/main.c
+++ week-02/day-2/Advanced_4/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <limits.h>
+
 int main()
 {
     //write a program which loads up fully a 50-sized integer array starting from 200 in descending order
@@ -6,6 +8,25 @@ int main()
     //print out the array
 
     int my_array[50];
+    int count = 0;
+
+    for (int i = 200; i < INT_MAX; i++) {
+
+        if (!(i % 85)) {
+            my_array[count] = i;
+            printf("#%d: Common divisor of 5 and 17 found at %d.\n", count, i);
+            count++;
+        }
+
+        if (count == 50) {
+            break;
+        }
+    }
+
+    for (int i = 0; i < sizeof(my_array) / sizeof(my_array[0]); i++) {
+
+        printf("Position #%d in my_array is %d\n", i, my_array[i]);
+    }
 
 
 
