b017fcdf495d02de90c3942b1a763af774c85cb4
Tamas Marsal <tamas.marsal@gmail.com>
2017-10-10 14:16:42 +0200

    styled Advanced_2 and initialized Advanced_3.

--- week-02/day-2/Advanced_2/main.c
+++ week-02/day-2/Advanced_2/main.c
@@ -4,21 +4,17 @@
 
 int main()
 {
-	int number_array[8] = {-48, -59, -2, 100, -2, -56, -78, -12};
+	int number_array[8] = {-48, -59, -2, -34, -2, -56, -78, -12};
 
 	int largest = INT_MIN;
 	int second_largest = INT_MIN;
 
 	for (int i = 0; i < sizeof(number_array) / sizeof(number_array[0]); i++) {
 
-	if (number_array[i] > second_largest && number_array[i] > largest) {
-
-        largest = number_array[i];
-	}
-	else if (number_array[i] > second_largest) {
-
-        second_largest = number_array[i];
-	}
+        if (number_array[i] > second_largest && number_array[i] > largest)
+            largest = number_array[i];
+        else if (number_array[i] > second_largest)
+            second_largest = number_array[i];
 	}
 
 	printf("largest: %d\n", largest);
