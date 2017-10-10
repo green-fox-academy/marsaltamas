bbd628606f244de9cd54972b7bd726edd8a0a6ad
Tamas Marsal <tamas.marsal@gmail.com>
2017-10-10 14:54:36 +0200

    completed working version of Advanced_3.

--- week-02/day-2/Advanced_3/main.c
+++ week-02/day-2/Advanced_3/main.c
@@ -1,14 +1,56 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
     int arr[20];
     int freq[20];
+    int real_arr_size = 0;
 
-    // TODO: write a program wich asks for a number,
+    // TODO: write a program which asks for a number,
     // that will be the real size of the array (20 elements are allocated, that's the maximum size)
+
+    printf("Please enter a size for an array between 1 and 20: ");
+    scanf("%d", &real_arr_size);
+
+    printf("\nReal arr size is %d\n\n", real_arr_size);
+
     // Then load up the array with integer values
+
+    for (int i = 0; i < real_arr_size; i++) {
+        printf("Please enter an element for position %d.: ", i);
+        scanf("%d", &arr[i]);
+    }
+
+    puts("\nThe inputed array is:");
+
+    for (int i = 0; i < real_arr_size; i++) {
+        printf("Position %d.: %d\n", i, arr[i]);
+    }
+
+    puts("\n");
+
     // Finally print all unique elements in array
+
+    int flag = 0;
+
+    for (int i = 0; i < real_arr_size; i++) {
+
+        for (int y = 0; y < real_arr_size; y++) {
+
+            if (arr[i] == arr[y])
+                flag++;
+
+                if(flag > 1)
+                    break;
+        }
+
+        if (flag < 2) {
+            printf("Position %d. = %d is unique in the array.\n", i, arr[i]);
+        }
+
+        flag = 0;
+    }
 
 
     return 0;
