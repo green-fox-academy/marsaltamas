6619843c86bfab41c8ac97bd311a283d9417e091
Tamas Marsal <tamas.marsal@gmail.com>
2017-10-11 11:59:55 +0200

    passed 2d array to functions.

--- week-02/day-3/Sieve of Eratosthenes/main.c
+++ week-02/day-3/Sieve of Eratosthenes/main.c
@@ -3,27 +3,29 @@
 #include <stdint.h>
 
 int give_me_limit(void);
-void array_printer(int arr[], int size, int step);
+void array_printer(int arr[][1], int size, int step);
 
 int main()
 {
-
+    // setting up limit
     int size = give_me_limit();
-    static int array[10000000];
+    static int array[10000000][1];
 
-
+    // loding array up to given limit
     for (int i = 0; i < size; i++) {
-        array[i] = i;
+        array[i][1] = 1;
     }
 
-    array_printer(array, size, 1000);
+
+
+    array_printer(array, size, 1);
 
 
 }
 
+// requests the user to input the upper limit up to which he wants to find primes
 int give_me_limit (void)
 {
-
     int size = 0;
 
     do {
@@ -34,13 +36,13 @@ int give_me_limit (void)
 
     return size;
 }
-
-void array_printer (int arr[], int size, int step)
+// prints array up to given size at the rate of give steps
+void array_printer (int arr[][1], int size, int step)
 {
 
     for (int i = 0; i < size; i = i + step) {
 
-        printf("Array element at pos #%d is %d.\n", i, arr[i]);
+        printf("Array element at pos #%d is %d.\n", i, arr[i][1]);
     }
 
 }
