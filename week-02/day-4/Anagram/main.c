3a51e9745332d7e228bba79e8f900a3ceb0c863c
Tamas Marsal <tamas.marsal@gmail.com>
2017-10-12 16:25:52 +0200

    working version of Anagram.

--- week-02/day-4/Anagram/main.c
+++ week-02/day-4/Anagram/main.c
@@ -2,73 +2,64 @@
 #include <stdlib.h>
 #include <string.h>
 
+void not_anagrams();
+
 int main()
 {
 
-    char word1[20];
-    char word2[20];
+    char word1[20] = "lasdf";
+    char word2[20] = "asee";
+    int flag = 1;
 
     puts("Please enter word1:");
     gets(word1);
     puts("Please enter word2:");
     gets(word2);
 
-    printf("word1 %s\n", word1);
-    printf("word2 %s\n", word2);
-
-    int flag = 0;
-    if (strlen(word1) != strlen(word2)) {
-        flag = 1;
-    }
-
-        /*pseudo:
-        checks equal length
-        picks letter from word1
-        checks if word2 has that letter
-        checks how many times it is in word1
-        if so, how many times word2 has that letter*/
-
-    // i-th letter of word1
     for (int i = 0; i < strlen(word1); i++) {
-        if (flag) {
-            printf("They are not anagrams.\n");
+
+        int word1count = 0;
+        int word2count = 0;
+
+        if (strlen(word1) != strlen(word2)) {
+            printf("message from non equal: They are not anagrams.\n");
+            not_anagrams();
+            flag = 0;
             break;
         }
-            // checks if word1[i] letter is available in word2
+
+        if (!strchr(word2, word1[i])) {
+            printf("message from letter not found: They are not anagrams.\n");
+            not_anagrams();
+            flag = 0;
+            break;
+        }
+
         for (int j = 0; j < strlen(word1); j++) {
-            // if word1[i] available in word2, counting the occurances of word1[i] in word1
+            //lettercount in word1
+            if (word1[i] == word1[j])
+                word1count++;
 
-            int count_at_word1 = 0; // variable to keep track of how many times we can see letter in word1
-            int count_at_word2 = 0; // variable to keep track of how many times we can see letter in word2
+            //lettercount in word2
+            if (word1[i] == word2[j])
+                word2count++;
+        }
 
-            if(word1[i] == word2[j]) {
-
-                for (int k = 0; k < strlen(word1); k++) {
-                    if (word1[i] == word1[k])
-                        count_at_word1++;
-                }
-
-                for (int l = 0; l < strlen(word1); l++) {
-                    if (word1[i] == word2[l])
-                        count_at_word2++;
-                }
-            }
-
-            if (count_at_word1 != count_at_word2) {
-                flag = 1;
-                break;
-            } else {
-                 count_at_word1 = 0;
-                 count_at_word2 = 0;
-            }
-
-
+        if (word1count != word2count) {
+            printf("message from letter count not matching: They are not anagrams.\n");
+            not_anagrams();
+            flag = 0;
+            break;
         }
     }
 
-    if (!flag)
+    if (flag)
         printf("They are anagrams.\n");
 
-
     return 0;
+}
+
+void not_anagrams()
+{
+   // printf("message from non equal: They are not anagrams.\n");
 }
