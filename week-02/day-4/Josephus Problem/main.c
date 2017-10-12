1b04594fa4f530fd22ebcfb6c61f76c0628006d9
Tamas Marsal <tamas.marsal@gmail.com>
2017-10-12 18:07:46 +0200

    working version of Josephus Problem.

--- week-02/day-4/Josephus Problem/main.c
+++ week-02/day-4/Josephus Problem/main.c
@@ -1,49 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// for checking results: http://webspace.ship.edu/deensley/mathdl/Joseph.html
+
 int main()
 {
-   int n_th = 2; // n_th person to be eliminated
-   int participants = 41;
+   int n_th = 7; // n_th person to be eliminated
+   int participants = 150;
+
+
 
    int arr[1000];
 
-   //loading array from 1 with flags: 0 = alive, 1 = dead
+   //loading array from 1 with flags: 1 = alive, 0 = dead
    for (int i = 1; i <= participants; i++) {
 
-        arr[i] = 0;
+        arr[i] = 1;
    }
 
-   int alive_counter = 2;
-   int n_th_counter = 0;
+   //printer
+    for (int i = 1; i <= participants; i++) {
 
-   int i = 0; // iterator in do-while loop
+        printf("arr[%d] = %d\n", i, arr[i]);
+   }
+
+
+   int alive_counter = 0;
+   int n_th_counter = 1;
 
    do {
 
+    alive_counter = 0;
+
     for (int j = 1; j <= participants; j++) {
 
-        if (!arr[j])
+        if (n_th_counter != n_th && arr[j]) {
             n_th_counter++;
-        else
             alive_counter++;
-
-        if (n_th_counter == n_th) {
-            arr[j] = 1;
-            n_th_counter = 0;
+        } else if (n_th_counter == n_th && arr[j]){
+            arr[j] = 0;
+            n_th_counter = 1;
         }
     }
-
-         if (alive_counter == 1)
-            break;
-        else
-            alive_counter = 0;
-
-   } while (alive_counter == 1);
+   } while (alive_counter != 1);
 
 
    for (int k = 1; k <= participants; k++) {
-        if (arr[k] == 0)
+        if (arr[k] == 1)
             printf("the one left alive was at position %d.", k);
            // break;
    }
