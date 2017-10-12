76edf5bbd0568287ed517d6d680f2fd8da02b7cd
Tamas Marsal <tamas.marsal@gmail.com>
2017-10-12 14:51:03 +0200

    build loops to find segments in passed in string for Plaindrom_searcher.

--- week-02/day-4/Palindrom_searcher/main.c
+++ week-02/day-4/Palindrom_searcher/main.c
@@ -1,13 +1,45 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 char *palindrom(char word1[]);
 
 int main()
 {
-    char word1[25] = "alma malma";
+    char word1[25] = "racecar";
+    char word2[25] = "racdecar";
+    char word3[25] = "racacar";
 
-    printf("the palindrom is %s", palindrom(word1));
+    printf("%d\n", strcmp(word1, palindrom(word1)));
+
+    printf("the word1 is %s\n", palindrom(word1));
+    printf("the palindrom is %s\n", palindrom(word1));
+
+    // length of checked segment
+    for (int length = 3; length <= strlen(word1); length++) {
+        //position of checked segment's first char
+        for (int position = 0; position < strlen(word1) - length + 1; position++) {
+            // reversing word segment and checking
+            printf("\nsegment to compare:\n");
+            for (int i = position; i < position + length; i++) {
+                printf("%c", word1[i]);
+            }
+            printf("\n---------\n");
+
+            int charcount = length;
+
+            for (int i = position; i <= length + length / 2; i++){
+
+                word2[length - charcount] = word1[i];
+                word3[charcount] = word1[i];
+            }
+            word2[length] = "\0";
+            word3[length] = "\0";
+
+            if (strcmp(word2, word3))
+                printf("You found a partial palindrom: %s\n", word2);
+        }
+    }
 
     return 0;
 }
