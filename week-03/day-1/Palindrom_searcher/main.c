e4d49851201806fe05810570c05819cd4729032f
marsaltamas <tamas.marsal@gmail.com>
2017-10-16 22:29:55 +0200

    debugged Plaindrom searcher.

---
+++ week-03/day-1/Palindrom_searcher/main.c
@@ -0,0 +1,36 @@
+#include <stdlib.h> // added to eliminate impl decl.
+#include <stdio.h>
+#include <string.h> // removed duplicated string.h include
+
+int is_palindrom(char *a);
+
+int main()
+{
+	char original_string[100], current_string[100];
+	printf("Enter the string to check if your can find palindrom substring is it\n"); // mod text to cover function
+	gets(original_string);
+
+	int i, j;
+	for (i = 0; i < strlen(original_string) - 1; i++) {
+		for (j = 2; i + j <= strlen(original_string); j++) { // added <=, so string checed all along
+			strncpy(current_string, original_string + i, j + 1);
+			current_string[j] = 0;
+			if (is_palindrom(current_string))
+				printf("%s\n", current_string);
+		}
+	}
+	return 0;
+}
+
+int is_palindrom(char *a)
+{
+
+	char b[100];
+	strcpy(b, a);
+	strrev(b);
+
+	if (strcmp(a, b) == 0)
+		return 1;
+	else
+		return 0;
+}
