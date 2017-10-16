c2c78e0bf84520e7a82b1cc4c5871a9eeadcc623
Tamas Marsal <tamas.marsal@gmail.com>
2017-10-16 15:53:04 +0200

    Square_checker solved to work.

--- week-03/day-1/Square_checker/main.c
+++ week-03/day-1/Square_checker/main.c
@@ -1,8 +1,41 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <math.h>
+
+// This program should work correctly with the following input: 20, 400
+// first case compiler optimizes out pow, so pow is not actually called
+// when calling pow, the actual double return result is rounded down when represented as in int, so i used round() function.
+// scanf reads double with lf placeholder
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+//	int number, result;
+//	printf("Please enter a number: ");
+//	fflush(stdin);
+//	scanf("%d", &number);
+//
+//	printf("Please enter the square of that number: ");
+//	fflush(stdin);
+//	scanf("%d", &result);
+//
+//	if ( round(pow(number, 2)) ==  result)
+//		printf(":)\n");
+//	else
+//		printf(":(\n");
+
+    // double variables and scanf used with lf scan
+    double numberd, resultd;
+	printf("Please enter a number: ");
+	fflush(stdin);
+	scanf("%lf", &numberd);
+
+	printf("Please enter the square of that number: ");
+	fflush(stdin);
+	scanf("%lf", &resultd);
+
+	if ( round(pow(numberd, 2)) ==  resultd)
+		printf(":)\n");
+	else
+		printf(":(\n");
+
+	return 0;
 }
