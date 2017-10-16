1c44672e2a5e90348ba6c934e8dd71ea125ea09d
Tamas Marsal <tamas.marsal@gmail.com>
2017-10-16 17:02:34 +0200

    started to functionalize Guess_game.

--- week-03/day-1/Guess_game/main.c
+++ week-03/day-1/Guess_game/main.c
@@ -6,40 +6,90 @@
 //project-guessing-game
 //Note: the program gets harder, when you set a lower range
 
+int scan_min(void);
+int scan_max(void);
+int rand_gen(int min, int max);
+int lives_calc(int min, int max);
+void start_game(int min, int max);
+void guess(int to_guess, int lives);
+
+
 int main()
 {
-	int x, y;
-	int count;
-	int guess = 0;
-	int rand_max;
-	int rand_min;
-	printf("please add the minimum number!\n");
-	scanf("%d", &rand_min);
-	printf("please add the maximum number!\n");
-	scanf("%d", &rand_max);
-	srand(time(NULL));
-	int rand_num = rand() % (rand_max + 1 - rand_min) + rand_min;
-	y = rand_max - rand_min;
-	x = log10(y) / log10(2) + 0;
+//	int x, y;
+//	int count;
+//	int guess = 0;
 
-	printf("I have a number between %d and %d. You have %d lives\n", rand_min, rand_max, x);
+//	int rand_max;
+//	int rand_min;
 
-	for (count = 1; count <= x; count++) {
-		printf("\n>");
-		scanf("%d", &guess);
+	start_game(scan_min(), scan_max());
 
-			if (guess == rand_num) {
-				printf("Yeeeeey, you did well! It took you %d tires!", count);
-				break;
-			} else if (guess > rand_num) {
-				printf("Too big, You have %d, lives left\n", x - count);
-			} else {
-				printf("Too low, You have %d, lives left\n", x - count);
-			}
-	}
-
-	if (count = x)
-		printf("You have used all your lives!");
+//	for (count = 1; count <= x; count++) {
+//		printf("\n>");
+//		scanf("%d", &guess);
+//
+//			if (guess == rand_num) {
+//				printf("Yeeeeey, you did well! It took you %d tires!", count);
+//				break;
+//			} else if (guess > rand_num) {
+//				printf("Too big, You have %d, lives left\n", x - count);
+//			} else {
+//				printf("Too low, You have %d, lives left\n", x - count);
+//			}
+//	}
+//
+//	if (count = x)
+//		printf("You have used all your lives!");
 
 	return 0;
 }
+
+int scan_min(void) {
+
+    int rand_min = 0;
+
+    printf("please add the minimum number!\n");
+	scanf("%d", &rand_min);
+
+	return rand_min;
+}
+
+int scan_max(void) {
+
+    int rand_max = 0;
+
+    printf("please add the maximum number!\n");
+	scanf("%d", &rand_max);
+
+	return rand_max;
+}
+
+int rand_gen(int min, int max)
+{
+    srand(time(NULL));
+	int rand_num = rand() % (max + 1 - min) + min;
+
+    return rand_num;
+}
+
+int lives_calc(int min, int max)
+{
+    int lives = 0;
+
+    int y = max - min;
+    lives = log10(y) / log10(2) + 1;
+
+    return lives;
+}
+
+void start_game(int max, int min)
+{
+    int lives = lives_calc(min, max);
+    int to_guess = rand_gen(min, max);
+    printf("I have a number between %d and %d. You have %d lives\n", min, max, lives);
+    printf("num to guess %d", to_guess);
+    //guess(to_guess, lives);
+}
+
+
