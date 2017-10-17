49dba47ef84985a8cbac5f828f717ebe2e1202fa
Tamas Marsal <tamas.marsal@gmail.com>
2017-10-17 10:11:15 +0200

    solved w3d2 task2.

---
+++ week-03/day-2/2/main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+	float x = 1.9;
+
+    //TODO: write a program wich adds up x and 1, and print out it rounded.
+
+    int one = 1;
+
+    printf("%.f", x + one);
+
+
+    return 0;
+}
