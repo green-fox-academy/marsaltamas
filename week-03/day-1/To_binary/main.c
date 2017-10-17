a338a56e15c30f8998b2328adb65ca6232f2d4f3
Tamas Marsal <tamas.marsal@gmail.com>
2017-10-17 09:15:09 +0200

    bugfix in To_binary.

--- week-03/day-1/To_binary/main.c
+++ week-03/day-1/To_binary/main.c
@@ -3,23 +3,32 @@
 
 void int_to_bin_digit(int in)
 {
-	int mask = (int) pow(2,(log10(in) / log10(2)));
+	int mask = (int) pow(2,(log10(in) / log10(2)) + 4);
 	int i;
 	int result = 0;
 
 	if (in == 0) {
          printf("%d", 0);
 	} else {
-        for (i = mask; i > 0; i/=2) {
+
+	    int space = 0;
+
+        for (i = mask; i >= 1; i/=2) {
             result = (in & mask) ? 1 : 0;
             printf("%d", result);
             mask = mask >> 1;
+
+            space++;
+
+            if (space % 4 == 0) {
+                printf(" ");
+            }
         }
 	}
 }
 
 int main()
 {
-	int_to_bin_digit(72);
+	int_to_bin_digit(8);
 	return 0;
 }
