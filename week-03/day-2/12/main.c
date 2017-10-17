197cc203236d69a192db899c4317fbad430e68cf
Tamas Marsal <tamas.marsal@gmail.com>
2017-10-17 15:16:29 +0200

    solved w3d2 task12.

--- week-03/day-2/12/main.c
+++ week-03/day-2/12/main.c
@@ -2,9 +2,25 @@
 #include <string.h>
 int main ()
 {
-	char string[55] ="This is a string for testing";
+	char string[55] = "This is a string for testing";
+	char to_find[5] = "i";
 	char *p;
 
 	//TODO: write a program, which lists all position where character 'i' is found
+
+	// using for loop w/manual search
+	for (int i = 0; i < strlen(string); i++) {
+
+        if (string[i] == 'i')
+            printf("Own \"function\": string[%d] is i.\n", i);
+	}
+
+	p = strpbrk(string, to_find);
+
+	while (p != 0) {
+        printf("strpbrk(): position was [%d]\n", p - string);
+        p = strpbrk(p + 1, to_find);
+	}
+
 	return 0;
 }
