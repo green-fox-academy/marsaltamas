4d584379882e73e454389426764064b6f0280feb
Tamas Marsal <tamas.marsal@gmail.com>
2017-10-17 11:10:07 +0200

    solved w3d2 task5.

---
+++ week-03/day-2/5/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+//TODO: write a function which returns 485 as unsigned integer value
+//put your return value in an integer variable in which you should have 229, and print it out
+//try to figure out which type casting you should use
+
+
+unsigned int fourhundred85(int input);
+
+int main()
+{
+
+    int val = fourhundred85(229);
+
+    printf("%d\n", val);
+
+    return 0;
+}
+
+unsigned int fourhundred85(int input)
+{
+    return input;
+}
