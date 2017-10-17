b1d19ced3b5d6dde4fa864a5451b2171f099bbf9
Tamas Marsal <tamas.marsal@gmail.com>
2017-10-17 10:02:06 +0200

    solved w3d2 task1.

---
+++ week-03/day-2/1/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <conio.h>
+
+main()
+{
+	int  i = 2;
+	float f = 13.5, result;
+
+	//TODO: print out the f/i quotient
+
+	printf("%.f", f / i);
+
+	//QUESTION: do you need explicit conversion?
+
+	// no, due to UAC
+
+
+	return 0;
+}
