00d2ff49d5b0a16a83bd7c7356cd3da293cd3270
Tamas Marsal <tamas.marsal@gmail.com>
2017-10-17 12:04:03 +0200

    initialized w3d2 task8.

---
+++ week-03/day-2/8/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void transform (char str[], int original_base, int new_base);
+
+//TODO: write a program, which transforms a number from a number system to another.
+//use the stdlib.h functions, and take care of error handling (see the reference)
+//the transform() function should print out the result.
+
+int main()
+{
+    char str[20];
+    int original_base;
+    int new_base;
+
+    printf("Give me a number to transform it in an other system: ");
+    scanf("%s", str);
+    printf("Give me the original base: ");
+    scanf("%d", &original_base);
+    printf("Give me the number of the new base: ");
+    scanf("%d", &new_base);
+
+    transform(str, original_base, new_base);
+    return(0);
+}
+
+void transform (char str[], int original_base, int new_base)
+{
+    long ret = 0;
+    ret = strtol(str, NULL, new_base);
+
+    printf("%d", ret);
+}
