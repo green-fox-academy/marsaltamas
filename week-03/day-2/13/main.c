d6b7551f79529efca87f1afa1b46497578da7e5d
Tamas Marsal <tamas.marsal@gmail.com>
2017-10-17 15:48:31 +0200

    working version for w3s2 task13.

--- week-03/day-2/13/main.c
+++ week-03/day-2/13/main.c
@@ -3,10 +3,15 @@
 
 int main ()
 {
-	char str[] = "This is a sample string";
+	char str[] = "This is a samp   le string";
     char *first, *last;
 
-	//TODO: write a program which prints out the distance between the first and last occurance of character 's'
+	//TODO: write a program which prints out the distance between the first and last occurrence of character 's'
+
+	first = strchr(str, 's');
+	last = strrchr(str, 's');
+
+	printf("first occurrence was: %d\nlast occurrence was %d\ndistance: %d", first - str, last - str, (last - str) - (first - str));
 
     return 0;
 }
