fadb6f758b7f96a9e2fd91b30e9f6370816c3b8f
Tamas Marsal <tamas.marsal@gmail.com>
2017-10-22 20:02:24 +0200

    task 6 completed.

---
+++ week-04/day-2/6.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int high_number = 6655;
+    int low_number = 2;
+
+    int *hight_number_pointer = &high_number;
+    int *other_high_number_pointer;
+    //TODO:
+    // The "other_high_number_pointer" should point to the same memory address
+    // without using the "&" operator.
+
+    other_high_number_pointer = hight_number_pointer;
+    //other_high_number_pointer = 6422272;
+
+    printf("value of hnp: %d\n", *hight_number_pointer);
+    printf("address of hnp: %p\n", hight_number_pointer);
+
+
+    printf("value of ohnp: %d\n", *other_high_number_pointer);
+    printf("address of ohnp: %p\n", other_high_number_pointer);
+
+
+    return 0;
+}
