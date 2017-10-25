ba163a5d0f8814b516dac38e13b88506fa5c927c
Tamas Marsal <tamas.marsal@gmail.com>
2017-10-25 16:11:50 +0200

    commented intersecfinder (task 09).

--- week-04/day-3/09.c
+++ week-04/day-3/09.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//TODO: write a C program which can find the intersection point of two section
+// RESULTS: http://www.ambrsoft.com/MathCalc/Line/TwoLinesIntersection/TwoLinesIntersection.htm
+
+/* f(x) = ax + c;
+ * g(x) = bx + d; a, b = slope
+*/
+
 typedef struct
 {
     float x;
@@ -14,12 +21,6 @@ typedef struct
     point_t end;
 
 } section_t;
-
-typedef struct
-{
-    point_t p;
-    float right_side;
-} equation_t;
 
 float intersection_point_x(section_t sec1, section_t sec2)
 {
@@ -43,7 +44,6 @@ float intersection_point_y(section_t sec1, section_t sec2)
     float y = (a * d - b * c) / (a - b);
 
     return y;
-
 }
 
 point_t intersection_point(float x, float y)
@@ -56,22 +56,19 @@ point_t intersection_point(float x, float y)
     return intersection;
 }
 
-//TODO: write a C program which can find the intersection point of two section
-// RESULTS: http://www.ambrsoft.com/MathCalc/Line/TwoLinesIntersection/TwoLinesIntersection.htm
-
 int main()
 {
     section_t first_sect, second_sect;
 
-    first_sect.start.x = -8;
+    first_sect.start.x = 90;
     first_sect.start.y = 1;
     first_sect.end.x = 2;
-    first_sect.end.y = 5;
+    first_sect.end.y = -23;
 
     second_sect.start.x = -4;
     second_sect.start.y = 9;
-    second_sect.end.x = 2;
-    second_sect.end.y = -3;
+    second_sect.end.x = 45;
+    second_sect.end.y = 3;
 
     float x = intersection_point_x(first_sect, second_sect);
     float y = intersection_point_y(first_sect, second_sect);
