5c8b5c9f104ae12498855b903549e506d4563f1d
Tamas Marsal <tamas.marsal@gmail.com>
2017-10-30 19:27:57 +0100

    init: struc_functions00, 01, 02, working version of 00.

---
+++ week-05/day-1/struc_functions00.c
@@ -0,0 +1,79 @@
+/*
+ * Create a structure representing the data of a book:
+ *    * Title
+ *    * Author
+ *    * Year of publication
+ *
+ * Create functions for:
+ *    * Changing or setting the Title
+ *    * Creating a new book.
+ *    * Changing or setting the Author
+ *    * Changing or setting the Year of publication
+ *    * Getting each of these separately by a single function each.
+ *
+ * IMPORTANT: The first parameter of each function should be a "Book", a variable of the type of this structure.
+ *
+ * Create a main where You demonstrate that each of these works and print out the necessary info.
+ * IMPORTANT: You should only print from the main function. All other functions should return the info needed.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef struct Book {
+    char title[30];
+    char author[50];
+    int publication;
+} t_book;
+
+void book_printer(t_book book)
+{
+    printf("title: %s\n", book.title);
+    printf("author: %s\n", book.author);
+    printf("publication year: %d\n", book.publication);
+    printf("-----------------------\n");
+}
+
+t_book create_new_book(char title[], char author[], int publication)
+{
+    t_book new_book;
+
+    strcpy(new_book.title, title);
+    strcpy(new_book.author, author);
+    new_book.publication = publication;
+
+    return new_book;
+}
+
+void title_set(t_book *book, char new_title[])
+{
+    strcpy((*book).title, new_title);
+}
+
+void author_set(t_book *book, char new_author[])
+{
+    strcpy(book->author, new_author);
+}
+
+void publication_set(t_book *book, int new_publ_year)
+{
+    (*book).publication = new_publ_year;
+}
+int main()
+{
+
+    t_book c_programming = create_new_book("c Programming for dummies", "c. richie", 1991);
+    book_printer(c_programming);
+
+    title_set(&c_programming, "c programming at green fox");
+    book_printer(c_programming);
+
+    author_set(&c_programming, "Dani, Zsolti, Levi, Sami, Bence, Zoli");
+    book_printer(c_programming);
+
+    publication_set(&c_programming, 2017);
+    book_printer(c_programming);
+
+    return 0;
+}
---
+++ week-05/day-1/struc_functions01.c
@@ -0,0 +1,5 @@
+/*
+ * Take exercise structures_functions_00.
+ * Create an additional structure: Author. This should store Given name and Surname of the author.
+ * Rewrite the previous exercise to work with this new structure.
+ */
