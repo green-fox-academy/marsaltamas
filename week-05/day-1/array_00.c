741d534eb2b79345bb92e1950332442f22c18605
Tamas Marsal <tamas.marsal@gmail.com>
2017-10-30 12:34:15 +0100

    Practice: array_00.c working version.

---
+++ week-05/day-1/array_00.c
@@ -0,0 +1,48 @@
+/*
+ * Create an array of 5 integers.
+ * Fill it with random integer numbers.
+ * Print out the array elements in one line, divided by " | ".
+ * Reverse the order of the numbers _in_the_array_ an print them out again.
+ */
+
+ #include <stdlib.h>
+ #include <time.h>
+
+ void array_printer(int array[], int size);
+
+ int main()
+ {
+
+     int array[5];
+     int temp = 0;
+
+     srand(time(0));
+
+     for (int i = 0; i < 5; i++) {
+
+        array[i] = rand();
+     }
+
+     array_printer(array, 5);
+
+     for (int i = 0; i < 5 / 2; i++) {
+        temp = array[i];
+        array[i] = array[5 - i - 1];
+        array[5 - i - 1] = temp;
+     }
+
+     printf("\n");
+
+     array_printer(array, 5);
+
+
+    return 0;
+ }
+
+  void array_printer(int array[], int size)
+  {
+        for (int i = 0; i < size; i++) {
+
+        printf("%d | ", array[i]);
+    }
+  }
