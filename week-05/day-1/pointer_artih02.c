5b187f63627ae7feebfd737e3372b93bf8f6e954
Tamas Marsal <tamas.marsal@gmail.com>
2017-10-31 12:05:30 +0100

    Practice: working version of pointer_arith02.c

---
+++ week-05/day-1/pointer_artih02.c
@@ -0,0 +1,65 @@
+/*
+ * Create an array of integers. Make it 16 long. Fill it up with numbers as You wish. Print them out.
+ *     * Iterate over it with a pointer so, that You only get to each _second_ element. Print out only these.
+ *     * Iterate over it only touching each _fourth_ element. Print out only these.
+ *     * Iterate over it only touching each _eighth_ element. Print out only these.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+#define SIZE 16
+
+int main()
+{
+    int array[SIZE];
+    int *p = array;
+
+    srand(time(0));
+
+    for (int i = 0; i < SIZE; i++) {
+        *p = rand() % 100;
+        p++;
+    }
+
+    p = p - SIZE;
+
+    printf("printing every element:\n");
+    int i = 0;
+    while (i < SIZE) {
+         printf("array[%d]: %d\n", i, *p);
+         i++;
+         p++;
+    }
+
+
+    p = p - SIZE;
+    printf("printing every 2nd element:\n");
+    i = 0;
+    while (i < SIZE) {
+         printf("array[%d]: %d\n", i, *p);
+         i+= 2;
+         p+= 2;
+    }
+
+    p = p - SIZE;
+    printf("printing every 4th element:\n");
+    i = 0;
+    while (i < SIZE) {
+         printf("array[%d]: %d\n", i, *p);
+         i+= 4;
+         p+= 4;
+    }
+
+    p = p - SIZE;
+    printf("printing every 8th element:\n");
+    i = 0;
+    while (i < SIZE) {
+         printf("array[%d]: %d\n", i, *p);
+         i+= 8;
+         p+= 8;
+    }
+
+    return 0;
+}
