73db9b34654455176ba48ecfd36efa94112f0bbf
Tamas Marsal <tamas.marsal@gmail.com>
2017-11-13 14:41:24 +0100

    datatypes_vector_02.cpp working version.

---
+++ week-06/day-1/dataypes_vector_02.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+
+
+using namespace std;
+
+int main() {
+	//Create a vector of integers
+	//reserve place for 10 element, then fill those place with random numbers between 0 and 10
+
+    srand(time(0));
+
+	vector<int> rand_int_vector(10);
+
+	for (int i = 0; i < rand_int_vector.size(); i++) {
+        rand_int_vector.at(i) = rand() % 100;
+	}
+
+	for (int i = 0; i < rand_int_vector.size(); i++) {
+        cout << "rand_int_vector[" << i << "]: " << rand_int_vector[i] << endl;
+	}
+
+  return 0;
+}
