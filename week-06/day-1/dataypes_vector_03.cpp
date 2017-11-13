206f7f1f900a76b5ee2f2c7563025926f1e7c4f1
Tamas Marsal <tamas.marsal@gmail.com>
2017-11-13 15:42:38 +0100

    datatypes_vector_03.cpp working version.

---
+++ week-06/day-1/dataypes_vector_03.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include <random>
+#include <vector>
+
+//create a vector of integers with the size of 20
+//fill this vector with random numbers from 0 to 10
+//print the items of the vector
+//remove the even numbers, then print the items again
+
+using namespace std;
+
+int main() {
+
+	random_device generator;        // setting generator device
+	mt19937 engine(generator());    // seeding rand generator
+	uniform_int_distribution<int> distribution(0, 10); // setting range
+
+	vector<int> rand_int_vector(20);
+
+	// filling up vector
+	for (int i = 0; i < rand_int_vector.size(); ++i) {
+        rand_int_vector.at(i) = distribution(engine);
+	}
+
+	// printing vector
+    for (int i = 0; i < rand_int_vector.size(); ++i) {
+        cout << "rand_int_vector[" << i << "]: " << rand_int_vector.at(i) << endl;
+	}
+
+	cout << "-------------\n";
+
+	rand_int_vector.at(19) = 10;
+
+    cout << "replacing post 19: " << rand_int_vector.at(19) << endl << "-------" << endl;
+
+	// remove even numbers
+	vector<int>::const_iterator i;
+
+	for (i = rand_int_vector.begin(); i != rand_int_vector.end(); i++) {
+        if (!(*i % 2)) {
+            rand_int_vector.erase(i);
+            i--;
+        }
+	}
+
+    for (int i = 0; i < rand_int_vector.size(); ++i) {
+        cout << "rand_int_vector[" << i << "]: " << rand_int_vector.at(i) << endl;
+	}
+
+    return 0;
+}
