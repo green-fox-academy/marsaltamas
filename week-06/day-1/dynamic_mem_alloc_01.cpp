167fa5e6786fdcfe915143da37f716d9524557f5
Tamas Marsal <tamas.marsal@gmail.com>
2017-11-13 16:27:01 +0100

    mod: dynamic_mem_alloc_00.cpp, dynamic_mem_allco_01 init.

--- week-06/day-1/dynamic_mem_alloc_01.cpp
+++ week-06/day-1/dynamic_mem_alloc_01.cpp
@@ -5,9 +5,20 @@ using namespace std;
 // Please allocate a 5 long array and fill it with numbers from 0 to 4, then print the whole array
 // Please delete the array before the program exits
 
-int main() {
-  int* pointer = NULL;
+int main()
+{
 
+    int *pointer = NULL;
+    int size = 5;
 
-  return 0;
+    pointer = new int[size];
+
+    for (int i = 0; i < size; i++) {
+        *(pointer + i) = i;
+        cout << *(pointer + i) << endl;
+    }
+
+    delete []pointer;
+
+    return 0;
 }
---
+++ week-06/day-1/dynamic_mem_alloc_02.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/**
+ * Please create a program that asks for a count when it starts.
+ * It should ask for a number count times, then it shoud print the average of the nubmers.
+ * It should delete any dynamically allocated resource before the program exits.
+ */
+
+int main()
+{
+    int cnt = 0;
+    int *p = NULL;
+
+    cout << "Please enter how many numbers your will enter: " << endl;
+    cin >> cnt;
+
+    p = new int[cnt];
+
+    for (int i = 0; i < cnt; ++i) {
+        cout << "Please enter number nr." << i + 1 << ": " << endl;
+        cin >> *(p + i);
+    }
+
+    for (int i = 0; i < cnt; ++i) {
+        cout << "p[" << i + 1 << "]: " << *(p + i) << endl;
+    }
+
+    delete []p;
+
+    return 0;
+}
