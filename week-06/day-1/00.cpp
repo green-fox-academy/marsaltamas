3b8fdbdc556edb4ab67a31acf1b83f4c564605b4
Tamas Marsal <tamas.marsal@gmail.com>
2017-11-13 11:40:19 +0100

    00.cpp: working version.

---
+++ week-06/day-1/00.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+// Modify this program to greet you instead of the World!
+
+int main()
+{
+    string myName = "Tamas";
+
+    cout << "Hello " << myName + "!" << endl;
+
+    return 0;
+}
