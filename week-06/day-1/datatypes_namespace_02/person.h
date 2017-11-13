f795ea5b30dfda815dc5206bf02e7fcf2984ed85
Tamas Marsal <tamas.marsal@gmail.com>
2017-11-13 13:26:54 +0100

    datatypes_namespace_02.cpp working version

---
+++ week-06/day-1/datatypes_namespace_02/person.h
@@ -0,0 +1,16 @@
+
+namespace person {
+
+    string name = "Tamas";
+    int age = 31;
+    int favourite_nr = 3333;
+
+    void display()
+    {
+        cout << name << endl;
+        cout << age << endl;
+        cout << favourite_nr << endl;
+    }
+}
+
+
