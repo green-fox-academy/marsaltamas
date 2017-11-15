a0e5d1058344abae8faae275107e2741fcabbf4d
Tamas Marsal <tamas.marsal@gmail.com>
2017-11-15 18:05:19 +0100

    06 added get_size(), insert_before().

--- week-06/day-3/06/main.cpp
+++ week-06/day-3/06/main.cpp
@@ -21,7 +21,7 @@ class List {
             tail = NULL;
         }
 
-        void addNote(int value)
+        void addNode(int value)
         {
             Node *temp = new Node;
             temp->value = value;
@@ -30,16 +30,16 @@ class List {
             if (head == NULL) {
                 head = temp;
                 tail = temp;
-                //delete temp;
             } else {
                 tail->next = temp;
                 tail = tail->next;
-                //delete temp;
             }
         }
 
         void display_list()
         {
+            cout << "Printing list:" << endl;
+
             Node *temp = new Node;
             temp = head;
 
@@ -49,20 +49,65 @@ class List {
             }
         }
 
+        int get_size()
+        {
+            Node *temp = head;
+            int size_counter = 0;
+
+            while (temp != NULL) {
+                size_counter++;
+                temp = temp->next;
+            }
+
+            cout << "Running get_size: " << size_counter << endl;
+
+            return size_counter;
+        }
+
+        void insert_before(int pos, int value) // first element is pos 0th
+        {
+            int size = get_size();
+
+            if (size == 0) {
+                addNode(value);
+            } else {
+                Node *temp = new Node;
+                Node *new_node = new Node;
+                new_node->value = value;
+                new_node->next = NULL;
+                temp = head;
+
+                for (int i = 0; i < pos; ++i) {
+                    temp = temp->next;
+                }
+
+                new_node->next = temp->next;
+                temp->next = new_node;
+            }
+        }
+
+        void remove_at_position(int pos)
+        {
+
+        }
+
 };
 
 int main()
 {
-    List l;
+    List l; // init list
 
-    for (int i = 0; i < 5; ++i) {
-        l.addNote(i);
+    for (int i = 1; i <= 5; ++i) { // load list
+        l.addNode(i);
     }
 
-    cout << l.head->value << endl;
-    cout << l.tail->value << endl;
 
     l.display_list();
+    l.get_size();
+    l.insert_before(3, 100);
+    l.display_list();
+
+
 
     return 0;
 }
