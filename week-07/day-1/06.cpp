cbcdfb70aa611485826d204e5e66d8cc60cdb92d
Tamas Marsal <tamas.marsal@gmail.com>
2017-11-20 12:50:42 +0100

    init 06, 07.cpp working.

---
+++ week-07/day-1/06.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+// Create a function which throws a bulit-in exception
+// In the main(), put that function in a try block
+// Also in the main() catch, what your function throws
+
+int main() {
+
+
+	return 0;
+}
