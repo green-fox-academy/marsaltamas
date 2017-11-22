9d46b506b06e802e5a9e4e05b890f4ce55484915
Tamas Marsal <tamas.marsal@gmail.com>
2017-11-22 13:01:21 +0100

    queize engine wo multithreidng w7d1t14 working.

--- week-07/day-1/13 w kbhit.cpp
+++ week-07/day-1/13 w kbhit.cpp
@@ -2,47 +2,81 @@
 #include <ctime>
 #include <iostream>
 #include <conio.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-bool get_input ( char *buffer, std::size_t size, int timeout )
+using namespace std;
+
+// Create a simple program which has a time lock
+// The program shoul ask for a user input, but the user only has
+// 10 seconds to reply, if the user fails (reaches the time limit)
+// the program should print out: "failed, it took you too much time"
+// This program cloud be the engine of a quiz game.
+//
+// found the solution here (which is not using multiple threads):
+// https://stackoverflow.com/questions/421860/capture-characters-from-standard-input-without-waiting-for-enter-to-be-pressed
+// https://www.computerhope.com/forum/index.php?topic=104972.0
+
+
+/*
+ * this function takes a char array, a size for that array, and an int as time limit.
+ * returns true if time is over or the buffer is full or enter key hit was read from stream.
+ * returns false if timer is not over && the buffer is empty.
+ */
+bool get_input_in_due_time(char *buffer, size_t size, int timeout)
 {
-  std::time_t start = std::time ( 0 );
-  std::size_t n = 0;
+    time_t start = time (0);
+    size_t n = 0; // set current buffer position
 
-  for ( ; ; ) {
-    if ( n == 0 && std::difftime ( std::time ( 0 ), start ) >= timeout )
-      return false;
+    for ( ; ; ) {
+        if (difftime(time(0), start) >= timeout)
+            return false; // returning false if timed out and no keys were read from stream
 
-    if ( kbhit() ) {
-      if ( n == size - 1 )
-        break;
+        /*
+         * kbhit returns not 0 if a key was found in the stream.
+         * so this if block is skipped if stream is empty, otherwise stores the char found to the buffer
+         */
 
-      char ch = (int)getche();
+        if (kbhit()) {
+            if ( n == size - 1 )
+            break;
 
-      if ( ch == '\r' ) {
-        buffer[n++] = '\n';
-        break;
-      }
-      else
-        buffer[n++] = ch;
+            char ch = (int) getche();
+
+            if (ch == '\r') {
+                break;
+            } else {
+                buffer[n++] = ch;
+            }
+        }
     }
-  }
 
-  buffer[n] = '\0';
+    buffer[n] = '\0';
 
-  return true;
+    return true;
 }
 
 int main()
 {
-  char buffer[512] = {0};
+    char buffer[512] = "";
+    int guess = 3; // setting target number
 
-  if ( !get_input ( buffer, 512, 5 ) ) {
-    std::cout<<"Input timed out\n";
-    buffer[0] = '\n';
-  }
+    cout << "Please guess the number i was thinking off!\nSubmit your answer with pressing enters: ";
 
-  std::cout<<"input: \""<< buffer <<"\"\n";
+    if (!get_input_in_due_time(buffer, 512, 3)) {
+        cout << "\nSorry, time is over!";
+        strcpy(buffer, "There was no answer submitted.");
+    }
+
+    cout << "\nYour guess was: "  << buffer << endl;
+
+    if (atoi(buffer) == guess)
+        cout << "Correct guess!" << endl;
+    else
+        cout << "Have better luck next time! The number was 3, anyway." << endl;
+
+    return 0;
 }
 
-//https://stackoverflow.com/questions/421860/capture-characters-from-standard-input-without-waiting-for-enter-to-be-pressed
-//https://www.computerhope.com/forum/index.php?topic=104972.0
+
