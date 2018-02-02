0e3b68d101d1660833eedfce935084f521695060
Tamas Marsal <tamas.marsal@gmail.com>
2018-02-02 10:18:30 +0100

    added comments and styled.

--- week-16/FinalExamEntry/main.c
+++ week-16/FinalExamEntry/main.c
@@ -2,7 +2,7 @@
  * Final Exam Entry
  * Marsal Tamas
  * Task 7:
- *Create a function that takes a string and a letter,
+ * Create a function that takes a string and a letter,
  * than returns the count of occurrence of that letter in the string
  */
 
@@ -10,22 +10,29 @@
 #include <stdio.h>
 #include <string.h>
 
-int letter_in_string_num_timer(char string[], char char_to_look_up);
+/*
+ * returns the count of occurrence of
+ * param: char_to_look_up in
+ * param: string
+ * return: occurrence as int
+ * note: this function accepts only '\0' terminating strings
+ */
+int letter_in_string_num_times(char string[], char char_to_look_up);
 
 int main()
 {
-    char test_string[] = "a bb ccc dddd";
-    char char_to_look_up = 'k';
+    char test_string[] = "a bb ccc dddd 55555 A";
+    char char_to_look_up = 'A';
 
     printf("Chart to look up: %c\n"
            "Found in string: %s\n"
            "%d time(s)\n", char_to_look_up, test_string,
-           letter_in_string_num_timer(test_string, char_to_look_up));
+           letter_in_string_num_times(test_string, char_to_look_up));
 
     return 0;
 }
 
-int letter_in_string_num_timer(char string[], char char_to_look_up)
+int letter_in_string_num_times(char string[], char char_to_look_up)
 {
     int hit = 0;
 
