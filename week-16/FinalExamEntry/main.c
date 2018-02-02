c80a2be68eae0cf41b8154397c91ae0b1bb412b2
Tamas Marsal <tamas.marsal@gmail.com>
2018-02-02 09:50:22 +0100

    wrapped algo into function.

--- week-16/FinalExamEntry/main.c
+++ week-16/FinalExamEntry/main.c
@@ -1,28 +1,40 @@
-// Create a function that takes a string and a letter,
-// than returns the count of occurrence of that letter in the string
+/*
+ * Final Exam Entry
+ * Marsal Tamas
+ * Task 7:
+ *Create a function that takes a string and a letter,
+ * than returns the count of occurrence of that letter in the string
+ */
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
+int letter_in_string_num_timer(char string[], char char_to_look_up);
 
 int main()
 {
-
-    char test_string[] = "abbcccdddd kk";
+    char test_string[] = "a bb ccc dddd";
     char char_to_look_up = 'k';
 
-    printf("%s\n", test_string);
-    printf("%c\n", char_to_look_up);
+    printf("Chart to look up: %c\n"
+           "Found in string: %s\n"
+           "%d time(s)\n", char_to_look_up, test_string,
+           letter_in_string_num_timer(test_string, char_to_look_up));
 
+    return 0;
+}
+
+int letter_in_string_num_timer(char string[], char char_to_look_up)
+{
     int hit = 0;
 
-    for (int i = 0; i < strlen(test_string); i++) {
+    for (int i = 0; i < strlen(string); i++) {
 
-        if (test_string[i] == char_to_look_up) {
+        if (string[i] == char_to_look_up) {
             hit++;
-            printf("here: %d\n", hit);
         }
     }
 
-    return 0;
+    return hit;
 }
