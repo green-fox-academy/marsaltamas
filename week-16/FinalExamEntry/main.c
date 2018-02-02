dc39a6ae0bad4b2911edc44e4ddcda85c67c90e7
Tamas Marsal <tamas.marsal@gmail.com>
2018-02-02 09:43:11 +0100

    init algortihm.

--- week-16/FinalExamEntry/main.c
+++ week-16/FinalExamEntry/main.c
@@ -7,7 +7,22 @@
 
 int main()
 {
-    printf("hello\n");
+
+    char test_string[] = "abbcccdddd kk";
+    char char_to_look_up = 'k';
+
+    printf("%s\n", test_string);
+    printf("%c\n", char_to_look_up);
+
+    int hit = 0;
+
+    for (int i = 0; i < strlen(test_string); i++) {
+
+        if (test_string[i] == char_to_look_up) {
+            hit++;
+            printf("here: %d\n", hit);
+        }
+    }
 
     return 0;
 }
