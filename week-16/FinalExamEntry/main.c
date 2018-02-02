cc6dde874690c4beb53833f94d9e55d2babece92
Tamas Marsal <tamas.marsal@gmail.com>
2018-02-02 09:36:45 +0100

    init task 7.

---
+++ week-16/FinalExamEntry/main.c
@@ -0,0 +1,13 @@
+// Create a function that takes a string and a letter,
+// than returns the count of occurrence of that letter in the string
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main()
+{
+    printf("hello\n");
+
+    return 0;
+}
